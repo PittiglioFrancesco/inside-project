5c2fb206523ecdff21775c76a96782ea10d6aa87
Francesco Pittiglio <f.pittiglio@reply.it>
2022-04-05 14:16:04 +0200

    header changes

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,40 +1,20 @@
 import react from 'react';
 
 const Header = () => {
+
     return(
         <nav className="navbar navbar-expand-lg navbar-light bg-light">
             <div className="container-fluid">
-                <span className="navbar-brand">realPage</span>
+                <span href="/" className="navbar-brand">realPage</span>
                 <button className="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
-                <span className="navbar-toggler-icon"></span>
+                    <span className="navbar-toggler-icon"></span>
                 </button>
                 <div className="collapse navbar-collapse" id="navbarSupportedContent">
-                <ul className="navbar-nav me-auto mb-2 mb-lg-0">
-                    <li className="nav-item">
-                        <span className="nav-link active" aria-current="page">Articoli</span>
-                    </li>
-                    <li className="nav-item">
-                    <span className="nav-link">Link</span>
-                    </li>
-                    <li className="nav-item dropdown">
-                    <span className="nav-link dropdown-toggle" id="navbarDropdown" role="button" data-bs-toggle="dropdown" aria-expanded="false">
-                        Dropdown
-                    </span>
-                    <ul className="dropdown-menu" aria-labelledby="navbarDropdown">
-                        <li><span className="dropdown-item">Action</span></li>
-                        <li><span className="dropdown-item">Another action</span></li>
-                        <li><hr className="dropdown-divider" /></li>
-                        <li><span className="dropdown-item">Something else here</span></li>
+                    <ul className="navbar-nav me-auto mb-2 mb-lg-0">
+                        <li className="nav-item">
+                            <a href="/articles" className="nav-link active" aria-current="page">Articoli</a>
+                        </li>
                     </ul>
-                    </li>
-                    <li className="nav-item">
-                    <span className="nav-link disabled">Disabled</span>
-                    </li>
-                </ul>
-                <form className="d-flex">
-                    <input className="form-control me-2" type="search" placeholder="Search" aria-label="Search" />
-                    <button className="btn btn-outline-success" type="submit">Search</button>
-                </form>
                 </div>
             </div>
         </nav>
